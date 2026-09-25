e3f8e01bf7a281864666cb395bcc1031ababdd8d
Priyanshu-Singh-25 <priyanshusinghh18@gmail.com>
2026-09-25 23:21:56 +0530

    Armstrong Numbers

---
+++ C++ Introduction/Armstrong Numbers.cpp
@@ -0,0 +1,40 @@
+//Problem
+
+      /*You are given a 3-digit number n, Find whether it is an Armstrong number or not.
+      
+      An Armstrong number of three digits is a number such that the sum of the cubes of its digits is equal to the number itself. 371 is an Armstrong number since 33 + 73 + 13 = 371. 
+      
+      Examples:
+      
+      Input: n = 153
+      Output: true
+      Explanation: 153 is an Armstrong number since 13 + 53 + 33 = 153. 
+      Input: n = 372
+      Output: false
+      Explanation: 372 is not an Armstrong number since 33 + 73 + 23 = 378. 
+      Input: n = 100
+      Output: false
+      Explanation: 100 is not an Armstrong number since 13 + 03 + 03 = 1. 
+      Constraints:
+      
+      100 ≤ n < 1000*/
+
+//Solution
+
+class Solution {
+  public:
+    bool armstrongNumber(int n) {
+        int m=n;
+        int sum=0;
+        for(int i=1; i<=m; m=m/10){
+            int p=m%10;
+            sum+=p*p*p;
+        }
+        if(sum==n){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+};
